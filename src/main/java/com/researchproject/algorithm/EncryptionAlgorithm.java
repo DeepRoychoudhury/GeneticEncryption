cd508d45f3ed40d3ecbe019a265dbca752033207
DeepRoychoudhury <deeproydrc@gmail.com>
2020-10-22 17:18:09 +0100

    Added crossover at the end of binary, added AES.

--- src/main/java/com/researchproject/algorithm/EncryptionAlgorithm.java
+++ src/main/java/com/researchproject/algorithm/EncryptionAlgorithm.java
@@ -5,28 +5,28 @@ import java.util.List;
 
 public class EncryptionAlgorithm {
 
-	//Visible encrypt method
+	//Visible encryption method
 	public String encrypt(String data) {
 		return null;
 	}
 	
-	//Bit calculation of integer n
+	//Bit calculation of integer n, Time Complexity: O(1)
 	private int BitManipulation(int n) {		
 		return Integer.bitCount(n);		
 	}
 	
-	//ASCII conversion of character to integer
+	//ASCII conversion of character to integer, Time Complexity: O(1)
 	private int AsciiValue(char data) {
 		return (int) data;
 	}
 	
 	//Generate pseudo random number using multiplicative congruential generator(MCG) 
-	//or Lehmer RNG {X(i+1)=(X(i)*a) modulo m}
+	//or Lehmer RNG {X(i+1)=(X(i)*a) modulo m}, Time Complexity : O(1)
 	private int crossovernumber(List<Integer> values) {
 		return ((values.get(0) * values.get(1)) % (values.get(2)));
 	}
 	
-	//Perform mutation of adjacent binary numbers for crossovers {2,1 and 0}
+	//Perform mutation of adjacent binary numbers for crossovers {2,1 and 0}, Time Complexity: Best Case: O(1), Worst Case: O(4)
 	private static List<List<Integer>> mutation(int cross, List<Integer> firstbinary, List<Integer> secondbinary){
 		List<List<Integer>> mutatedValue = new ArrayList<List<Integer>>();
 		if(cross==2) {
@@ -48,6 +48,10 @@ public class EncryptionAlgorithm {
 			firstbinary=secondbinary;
 			secondbinary=temp;
 		}	
+		
+		//Adding the crossover at the end of binary number
+		firstbinary.add(cross);
+		secondbinary.add(cross);
 		mutatedValue.add(firstbinary);
 		mutatedValue.add(secondbinary);
 		return mutatedValue;
